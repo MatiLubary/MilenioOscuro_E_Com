4f4dc306b7c3d9fc43981d44cd723ea32702b99b
LeoAlias <leo.alias@gmail.com>
2020-06-24 21:54:30 -0300

    agregue ruta controlador y vista login

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const userControllers = require ('../Controllers/userControllers')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/login', userControllers.login)
 
 module.exports = router;
