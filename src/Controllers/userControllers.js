96a0bfcae62fda152e165c656381c9569fac5fa7
LeoAlias <leo.alias@gmail.com>
2020-07-18 00:30:35 -0300

    se agrego validacion al register

--- src/Controllers/userControllers.js
+++ src/Controllers/userControllers.js
@@ -1,4 +1,6 @@
 let fs = require('fs');
+let {check, validationResult, body} = require ('express-validator');
+const { log } = require('console');
 
 userControllers = {
 
@@ -13,25 +15,38 @@ userControllers = {
         res.render('users/register')
     },
     create: function (req, res, next) {
-        let user = {
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        }
-        let archivoUser = fs.readFileSync('src/data/users.json', {
-            encoding: 'utf-8'
-        });
-        let users;
-        if (archivoUser == "") {
-            users = [];
-        } else {
-            users = JSON.parse(archivoUser);
-        }
+        let errors = (validationResult(req));
+        console.log(errors);
+        if (errors.isEmpty()){
+            let user = {
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            }
+            let archivoUser = fs.readFileSync('src/data/users.json', {
+                encoding: 'utf-8'
+            });
+            let users;
+            if (archivoUser == "") {
+                users = [];
+            } else {
+                users = JSON.parse(archivoUser);
+            }
 
-        users.push(user);
-        usersJson = JSON.stringify(users);
-        fs.writeFileSync('src/data/users.json', usersJson);
-        res.redirect('/')
+            users.push(user);
+            usersJson = JSON.stringify(users);
+            fs.writeFileSync('src/data/users.json', usersJson);
+            
+            res.redirect('/')
+        } 
+        else {
+            return res.render ('users/register', {errors:errors.errors})
+        }    
+    },
+    profile: function (req, res, next) {
+
+
+        res.render('users/profile')
     }
 }
 module.exports = userControllers;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,17 +1,24 @@
 var express = require('express');
 var router = express.Router();
 const userControllers = require ('../Controllers/userControllers')
+let {check, validationResult, body} = require ('express-validator')
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send("hola")
+  res.redirect('users/login')
 });
 
 router.get('/login', userControllers.login);
 
 router.get('/register', userControllers.register);
 
-router.post('/register', userControllers.create);
+router.post('/register', [
+  check('name').isLength({min:3}).withMessage('El Nombre debe contener por lo menos 3 letras'),
+  check('email').isEmail().withMessage('El Email debe ser una direccion valida'),
+  check('password').isLength({min:8}).withMessage('El Nombre debe contener por lo menos 8 caracteres') ], userControllers.create);
+
+router.get('/profile', userControllers.profile);
 
 
 
