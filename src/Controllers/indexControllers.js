1ffba3e61d377c4f618cfb936d7972bf93f05e6f
LeoAlias <leo.alias@gmail.com>
2020-07-29 22:30:19 -0300

    se agregan paginas footer y validacion

--- src/Controllers/indexControllers.js
+++ src/Controllers/indexControllers.js
@@ -28,6 +28,18 @@ controllerIndex = {
 
   cart: function (req, res, next) {
     res.render('index/cart' , {usuario : req.session.usuario})
+  },
+  contact: function (req, res, next){
+    res.render('index/contact' , {usuario : req.session.usuario})
+  },
+  envios: function (req, res, next){
+    res.render('index/envios' , {usuario : req.session.usuario})
+  },
+  quienes: function (req, res, next){
+    res.render('index/quienes' , {usuario : req.session.usuario})
+  },
+  metodopago: function (req, res, next){
+    res.render('index/metodopago' , {usuario : req.session.usuario})
   }
 
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -17,7 +17,7 @@ router.post('/login', [
 
 router.get('/register', userControllers.register);
 router.post('/register', [
-  check('name').isLength({min:3}).withMessage('El Nombre debe contener por lo menos 3 letras'),
+  check('name').isLength({min:3, max:15}).withMessage('El Nombre debe contener por lo menos 3 letras y maximo 15'),
   check('email').isEmail().withMessage('El Email debe ser una direccion valida'),
   check('password').isLength({min:8}).withMessage('La contraseña debe contener por lo menos 8 caracteres') ], userControllers.create);
 
